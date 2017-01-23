7b3d06d4038f9ad1e5fd4d9f39b7316b6baac73f
ZebulonIII <tritium99@Hotmail.com>
2017-01-23 13:49:02 -0700

    Update ExpressionManager.h

--- Student_Code/ExpressionManager.h
+++ Student_Code/ExpressionManager.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "ExpressionManagerInterface.h"
+
+using namespace std;
+
+class ExpressionManager : public ExpressionManagerInterface {
+private:
+    
+public:
+    ExpressionManager() {}
+    bool isBalanced(string);
+    string postfixToInfix(string);
+    string infixToPostfix(string);
+    string postfixEvaluate(string);
+};
